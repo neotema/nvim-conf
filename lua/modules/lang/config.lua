c5179790784df33e043f1c2f3e44d756cee55755
qBnig <qbrian2009@yahoo.com>
2022-09-07 17:45:30 +0800

    config nvim-treesitter

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -4,10 +4,18 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
+    ensure_installed = { 'lua', 'rust', 'toml' },
+    auto_install = true,
     ignore_install = { 'phpdoc' },
     highlight = {
       enable = true,
+      additional_vim_regex_highlighting=false,
+    },
+    ident = { enable = true },
+    rainbow = {
+      enable = true,
+      extended_mode = true,
+      max_file_lines = nil,
     },
     textobjects = {
       select = {
