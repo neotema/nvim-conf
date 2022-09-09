b6704e6e276bc7531a5523cb1e17909c06f4c46d
qBnig <qbrian2009@yahoo.com>
2022-09-09 20:48:04 +0800

    set rust-tool plugin #2

--- lua/modules/rust/config.lua
+++ lua/modules/rust/config.lua
@@ -4,6 +4,10 @@ function config.rust_tool_nvim()
     if not packer_plugins['nvim-lspconfig'].loaded then
         vim.cmd([[packadd nvim-lspconfig]])
     end
+    local extension_path = '/Users/arch/codelldb-x86/extension/'
+    local codelldb_path = extension_path .. 'adapter/codelldb'
+    local liblldb_path = extension_path .. 'lldb/lib/liblldb.dylib'
+
     local rt = {
         server = {
             settings = {
@@ -21,24 +25,32 @@ function config.rust_tool_nvim()
                 },
             }
         },
+        dap = {
+            adapter = require('rust-tools.dap').get_codelldb_adapter(
+                codelldb_path, liblldb_path)
+        }
     }
     require('rust-tools').setup(rt)
 end
 
-function config.nvim_dap()
-  local dap = require('dap')
-  dap.adapters.codelldb = {
-    type = 'server',
-    port = "${port}",
-    executable = {
-      command = '/Users/arch/codelldb-x86/extension/adapter/codelldb',
-      args = { "--port", "${port}" },
-    }
-  }
-end
+function config.plenary_nvim() end
 
-function config.nvim_dap_ui()
-  require('dapui').setup()
-end
+function config.nvim_dap() end
+    -- require('dap').setup()
+  -- local dap = require('dap')
+  -- dap.adapters.codelldb = {
+    -- port = 18111,
+    -- type = 'server',
+    -- port = "${port}",
+    -- executable = {
+    --   command = '/Users/arch/codelldb-x86/extension/adapter/codelldb',
+    --   args = { "--port", "${port}" },
+    -- }
+  -- }
+-- end
+
+function config.nvim_dap_ui() end
+--   require('dapui').setup()
+-- end
 
 return config
--- lua/modules/rust/plugins.lua
+++ lua/modules/rust/plugins.lua
@@ -5,17 +5,28 @@ plugin({
     'simrat39/rust-tools.nvim',
     config = conf.rust_tool_nvim,
     after = 'nvim-lspconfig',
-    requires = { "neovim/nvim-lspconfig" },
+    requires = {
+      { "neovim/nvim-lspconfig" },
+      { 'nvim-lua/plenary.nvim' },
+    },
+})
+
+plugin({
+    'nvim-lua/plenary.nvim',
+    config = conf.plenary_nvim,
+    requires = { 'nvim-lua/popup.nvim', opt = true },
 })
 
 plugin({
   'mfussenegger/nvim-dap',
   config = conf.nvim_dap,
+  after = 'plenary.nvim',
   requires = { 'nvim-lua/plenary.nvim' },
 })
 
 plugin({
   'rcarriga/nvim-dap-ui',
   config = conf.nvim_dap_ui,
-  requires = { 'mfussenegger/nvim-dap' },
+  after = 'nvim-dap',
+  requires = { 'mfussenegger/nvim-dap', opt = true },
 })
