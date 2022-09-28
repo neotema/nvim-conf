4afb2b29310d12f7504309d9d2866537489136ec
qBnig <qbrian2009@yahoo.com>
2022-09-28 16:38:17 +0800

    update vim opts

--- lua/general.lua
+++ lua/general.lua
@@ -43,3 +43,18 @@ vim.opt.listchars:append("eol:↴")
 vim.opt.laststatus = 3
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- folding
+vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.opt.foldcolumn = '1'
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
