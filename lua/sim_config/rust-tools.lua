b4faecb05c7eb0e1d096171bab9762ba56fedadf
qBnig <qbrian2009@yahoo.com>
2022-09-28 16:38:16 +0800

    config rust-tools for debugging func

--- lua/sim_config/rust-tools.lua
+++ lua/sim_config/rust-tools.lua
@@ -6,18 +6,21 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local rt = require("rust-tools")
+local extension_path = "$HOME/codelldb-x86/extension/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 rt.setup({
   tools = {
     inlay_hints = {
       auto = true,
-      only_current_line = true,
+      -- only_current_line = true,
       -- whether to show parameter hints with the inlay hints or not
       -- default: true
       show_parameter_hints = false,
     },
     on_initialized = function()
-      -- ih.set_all()
+      ih.set_all()
     end,
   },
   server = {
@@ -37,5 +40,16 @@ rt.setup({
         { buffer = bufnr }
       )
     end,
+  },
+  dap = {
+    -- adapter = {
+    --   type = "executable",
+    --   command = "lldb-vscode",
+    --   name = "rt_lldb",
+    -- },
+    adapter = require("rust-tools.dap").get_codelldb_adapter(
+      codelldb_path,
+      liblldb_path
+    ),
   },
 })
